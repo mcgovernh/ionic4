a557700bd600dd4c16360fdd203350e9bec5a244
mcgovernh <mcgovern1969@gmail.com>
2023-03-18 15:28:25 +0000

    with buttons

--- mobdev-ionic-intro/src/app/pages/films/films.page.ts
+++ mobdev-ionic-intro/src/app/pages/films/films.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import{Router} from '@angular/router';
+import{NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-films',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmsPage implements OnInit {
 
-  constructor() { }
+  constructor(private navController:NavController,private router:Router) { }
 
   ngOnInit() {
   }
+
+  openDetails(){
+    this.router.navigateByUrl('tabs/films/42')
+  }
+  goToPlanets()
+    {this.navController.navigateRoot('tabs/planets')}
+  
 
 }
